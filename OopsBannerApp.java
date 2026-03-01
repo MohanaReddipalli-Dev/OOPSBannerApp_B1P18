412a29e03042b2a8ff44dad7ccfdcabbc1618d5c
MohanaReddipalli-Dev <mohanareddipalli@gmail.com>
2026-03-01 10:18:20 +0530

    UC4 printing Arrays&Loops

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,7 +1,7 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println(String.join(" ", " ** "," ** ","*** ","*** "));
-		System.out.println(String.join(" ", "*  *","*  *","* * ","*   "));
+		System.out.println(String.join(" ", "*  *","*  *","*  *","*   "));
 		System.out.println(String.join(" ", "*  *","*  *","*** ","*** "));
 		System.out.println(String.join(" ", "*  *","*  *","*   ","   *"));
 		System.out.println(String.join(" ", " ** "," ** ","*   ","*** "));
